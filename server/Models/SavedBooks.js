da3e91e5c8cb07e6caf2dd6d9a227ca6bf384df9
Het <het.parekh@somaiya.edu>
2022-11-15 22:34:33 +0530

    File Cleanup

--- server/Models/SavedBooks.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-const SavedBooksSchema = new mongoose({
-    google_book_id:{type:"String" , required:true , index:{unique:true}},
-    user:{type:mongoose.Schema.Types.ObjectId, ref:"User"}
-})
