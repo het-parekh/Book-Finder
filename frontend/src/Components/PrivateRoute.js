33d39daad898d847eb154bab0df0e3d161a8fdf5
Het <het.parekh@somaiya.edu>
2022-11-27 21:31:30 +0530

    Build Updated

--- frontend/src/Components/PrivateRoute.js
+++ frontend/src/Components/PrivateRoute.js
@@ -2,11 +2,7 @@ import { Outlet , Navigate } from "react-router-dom"
 
 function PrivateRoute(props){
     return(
-<<<<<<< HEAD
         props.auhtorizationStatus?<Outlet />:<Navigate to="/"  replace={true}/>
-=======
-        props.authorizationStatus?<Outlet />:<Navigate to="/" />
->>>>>>> dev
     )
 }
 export default PrivateRoute
